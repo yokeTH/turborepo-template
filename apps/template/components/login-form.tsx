b64d0801efb8332cb82d7b2d3175ed8ede17dd38
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-09-02 04:22:57 +0700

    fix: missing height and width

--- apps/template/components/login-form.tsx
+++ apps/template/components/login-form.tsx
@@ -123,6 +123,8 @@ export function LoginForm({
           </form>
           <div className='bg-muted relative hidden md:block'>
             <Image
+              height={506}
+              width={383}
               src='/placeholder.svg'
               alt='Image'
               className='absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale'
